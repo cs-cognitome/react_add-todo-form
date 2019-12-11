1d53b9fc73b99ad0c6b27fb65f49107e751bd780
cs-cognitome <gaya.scienza.v@gmail.com>
2019-12-11 20:32:27 +0200

    Updates

---
+++ precache-manifest.111f0ea7dd07500ebcb6ec44efe7c4ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed696dd01d558144a4b8b358bd45ee1f",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "7e51be2a0f9e9a1313db",
+    "url": "/react_add-todo-form/static/css/main.d36bb678.chunk.css"
+  },
+  {
+    "revision": "1f9ec614ed3123c9478f",
+    "url": "/react_add-todo-form/static/js/2.4a41f9c6.chunk.js"
+  },
+  {
+    "revision": "7e51be2a0f9e9a1313db",
+    "url": "/react_add-todo-form/static/js/main.9ca0322c.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
